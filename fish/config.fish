a0993c41c370d2cc8acaa5cf5fb722aae152bb3e
Andrey Demidenko <a.demidenko@cactus.vision>
2021-02-08 08:32:21 +0300

    update configs

--- fish/config.fish
+++ fish/config.fish
@@ -2,15 +2,18 @@ set -g theme_color_scheme zenburn
 set -g fish_color_autosuggestion 'a4d2ae'
 set -g VIRTUAL_ENV_DISABLE_PROMPT 1
 
-set -g PATH $PATH "/usr/local/bin/"
-
-set -gx PATH $PATH "/usr/local/opt/gnu-getopt/bin"
+set -gx PATH "/usr/local/sbin" $PATH
+set -gx PATH "/usr/local/bin/" $PATH
+set -gx PATH "/usr/local/opt/gnu-getopt/bin" $PATH
 
 # haskell
-set -gx PATH $PATH "$HOME/.ghcup/bin"
-set -gx PATH $PATH "$HOME/.cabal/bin/"
-set -gx PATH $PATH "$HOME/.local/bin/"
+set -gx PATH "$HOME/.ghcup/bin" $PATH
+set -gx PATH "$HOME/.cabal/bin/" $PATH
+set -gx PATH "$HOME/.local/bin/" $PATH
 
-#icu4c
+# icu4c
 set -g LDFLAGS "-L/usr/local/opt/icu4c/lib"
 set -g CPPFLAGS "-I/usr/local/opt/icu4c/include"
+
+# neo4j
+#set -g NEO4J_HOME "$HOME/workspace/neo4j"
